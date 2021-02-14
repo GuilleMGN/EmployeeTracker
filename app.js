0c7da86c137bf04e1abfdeec452955986404776f
Matthew Guillen <matthewguillen777@gmail.com>
2021-02-14 10:55:44 -0500

    add seeds, util, table, and functions

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const util = require("util");
+const inquirer = require("inquirer");
+const { printTable } = require("console-table-printer");
 
 // Create the connection information for the SQL database
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -17,6 +19,8 @@ connection.connect(function (err) {
     start();
 });
 
+connection.query = util.promisify(connection.query);
+
 // Initial function which prompts the user for what action they should take
 function start() {
     inquirer
@@ -24,7 +28,7 @@ function start() {
             name: "action",
             type: "list",
             message: "What would you like to do? ",
-            choices: ["View All Employees", "Add Employee", "Remove Employee", "Update Manager", "Exit"]
+            choices: ["View All Employees", "Add Employee", "Remove Employee", "Update Manager", "View Departments", "View Roles", "Create Department", "Add Role", "Exit"]
         })
         .then((data) => {
             // Based on user answer, call the appropriate functions
@@ -41,6 +45,18 @@ function start() {
                 case "Update Manager":
                     updateManager();
                     break;
+                case "View Departments":
+                    viewDepartments();
+                    break;
+                case "View Roles":
+                    viewRoles();
+                    break;
+                case "Create Department":
+                    createDepartment();
+                    break;
+                case "Add Role":
+                    addRoles();
+                    break;
                 default: connection.end();
             }
         });
@@ -49,20 +65,83 @@ function start() {
 // function to view all employees
 function viewEmployees() {
     // Display chart in console.table()
-    console.log("");
-    connection.query("SELECT employee.id, " +
-    "employee.first_name AS FirstName, " +
-    "employee.last_name AS LastName, " +
-    "title AS Title, " +
-    "department_name AS Department, " +
-    "salary AS Salary, " +
-    "employee.manager_id AS Manager " + 
-    "FROM employee " + 
-    "LEFT JOIN employee ON employee.manager_id = employee.id " + 
-    "INNER JOIN role ON employee.role_id = role.id " + 
-    "INNER JOIN department ON role.department_id = department.id ", (err, res) => {
-        if (err) throw err;
-        console.table(res);
+    connection.query("SELECT * FROM employee").then(res => {
+        console.log("\n");
+        printTable(res);
         start();
     });
 }
+
+function viewDepartments() {
+    // Display chart in console.table()
+    connection.query("SELECT * FROM department").then(res => {
+        console.log("\n");
+        printTable(res);
+        start();
+    });
+}
+
+function viewRoles() {
+    // Display chart in console.table()
+    connection.query("SELECT * FROM role").then(res => {
+        console.log("\n");
+        printTable(res);
+        start();
+    });
+}
+
+function createDepartment() {
+    inquirer.prompt([{
+        name: "department",
+        type: "input",
+        message: "What is the name of the new department? "
+    }]).then(data => {
+        connection.query("INSERT INTO department SET department_name=?", data.department).then(res => {
+            console.log(res);
+            viewDepartments();
+        });
+    })
+}
+async function addRoles() {
+    const departments = await connection.query("SELECT * FROM department");
+    console.log(departments);
+    const deptArray = departments.map(({ department_name, id }) => ({
+        name: department_name,
+        value: id
+    }
+    ))
+    inquirer.prompt([
+        {
+            name: "title",
+            type: "input",
+            message: "What is the title for this role? "
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary for the new role? "
+        },
+        {
+            name: "department",
+            type: "list",
+            message: "What department is this role assigned to?",
+            choices: deptArray
+        }
+    ]).then(data => {
+        console.log(data);
+    })
+}
+
+// connection.query("SELECT employee.id, " +
+// "employee.first_name AS FirstName, " +
+// "employee.last_name AS LastName, " +
+// "title AS Title, " +
+// "department_name AS Department, " +
+// "salary AS Salary, " +
+// "employee.manager_id AS Manager " + 
+// "FROM employee " + 
+// "LEFT JOIN employee ON employee.manager_id = employee.id " + 
+// "INNER JOIN role ON employee.role_id = role.id " + 
+// "INNER JOIN department ON role.department_id = department.id ", (err, res) => {
+//     if (err) throw err;
+//     console.table(res);
