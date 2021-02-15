54589953e41446ab5d23fd4ad6af6354a76a2394
Matthew Guillen <matthewguillen777@gmail.com>
2021-02-15 15:54:43 -0500

    added main menu and sub menus inquirer

--- app.js
+++ app.js
@@ -15,51 +15,142 @@ const connection = mysql.createConnection({
 // Connect to the MySQL server and SQL database
 connection.connect(function (err) {
     if (err) throw err;
-    // Run the start function after the connection is made to prompt the user
-    start();
+    // Run the main menu function after the connection is made to prompt the user
+    mainMenu();
 });
 
 connection.query = util.promisify(connection.query);
 
 // Initial function which prompts the user for what action they should take
-function start() {
-    inquirer
-        .prompt({
+function mainMenu() {
+    inquirer.prompt([
+        {
             name: "action",
             type: "list",
             message: "What would you like to do? ",
-            choices: ["View All Employees", "Add Employee", "Remove Employee", "Update Manager", "View Departments", "View Roles", "Create Department", "Add Role", "Exit"]
-        })
-        .then((data) => {
-            // Based on user answer, call the appropriate functions
-            switch (data.action) {
-                case "View All Employees":
-                    viewEmployees();
-                    break;
-                case "Add Employee":
-                    addEmployee();
-                    break;
-                case "Remove Employee":
-                    removeEmployee();
-                    break;
-                case "Update Manager":
-                    updateManager();
-                    break;
-                case "View Departments":
-                    viewDepartments();
-                    break;
-                case "View Roles":
-                    viewRoles();
-                    break;
-                case "Create Department":
-                    createDepartment();
-                    break;
-                case "Add Role":
-                    addRoles();
-                    break;
-                default: connection.end();
-            }
-        });
+            choices: ["Add", "View", "Update", "Delete", "Exit"]
+        }
+    ]).then((data) => {
+        // Based on user answer, call the appropriate functions
+        switch (data.action) {
+            case "Add":
+                addMenu();
+                break;
+            case "View":
+                viewMenu();
+                break;
+            case "Update":
+                updateMenu();
+                break;
+            case "Delete":
+                deleteMenu();
+                break;
+            default: connection.end();
+        }
+    });
+}
+
+function addMenu() {
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to add? ",
+            choices: ["Add Department", "Add Role", "Add Employee"]
+        }
+    ]).then((data) => {
+        // Based on user answer, call the appropriate functions
+        switch (data.action) {
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            default: connection.end();
+        }
+    });
+}
+
+function viewMenu() {
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to view? ",
+            choices: ["View Departments", "View Roles", "View Employees"]
+        }
+    ]).then((data) => {
+        // Based on user answer, call the appropriate functions
+        switch (data.action) {
+            case "View Departments":
+                viewDepartments();
+                break;
+            case "View Roles":
+                viewRoles();
+                break;
+            case "View Employees":
+                viewEmployees();
+                break;
+            case "View Employees By Manager":
+                viewEmployeesByManager();
+                break;
+            case "View Departments Total Budget":
+                viewBudget();
+                break;
+            default: connection.end();
+        }
+    });
+}
+
+function updateMenu() {
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to update? ",
+            choices: ["Update Roles", "Update Managers"]
+        }
+    ]).then((data) => {
+        // Based on user answer, call the appropriate functions
+        switch (data.action) {
+            case "Update Roles":
+                updateRoles();
+                break;
+            case "Update Managers":
+                updateManagers();
+                break;
+            default: connection.end();
+        }
+    });
+}
+
+function deleteMenu() {
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to delete? ",
+            choices: ["Delete Departments", "Delete Roles", "Delete Employees"]
+        }
+    ]).then((data) => {
+        // Based on user answer, call the appropriate functions
+        switch (data.action) {
+            case "Delete Departments":
+                deleteDepartments();
+                break;
+            case "Delete Roles":
+                deleteRoles();
+                break;
+            case "Delete Employees":
+                deleteEmployees();
+                break;
+            default: connection.end();
+        }
+    });
 }
 
 // function to view all employees
@@ -68,7 +159,7 @@ function viewEmployees() {
     connection.query("SELECT * FROM employee").then(res => {
         console.log("\n");
         printTable(res);
-        start();
+        mainMenu();
     });
 }
 
@@ -77,7 +168,7 @@ function viewDepartments() {
     connection.query("SELECT * FROM department").then(res => {
         console.log("\n");
         printTable(res);
-        start();
+        mainMenu();
     });
 }
 
@@ -86,11 +177,11 @@ function viewRoles() {
     connection.query("SELECT * FROM role").then(res => {
         console.log("\n");
         printTable(res);
-        start();
+        mainMenu();
     });
 }
 
-function createDepartment() {
+function addDepartment() {
     inquirer.prompt([{
         name: "department",
         type: "input",
@@ -102,7 +193,7 @@ function createDepartment() {
         });
     })
 }
-async function addRoles() {
+async function addRole() {
     const departments = await connection.query("SELECT * FROM department");
     console.log(departments);
     const deptArray = departments.map(({ department_name, id }) => ({
